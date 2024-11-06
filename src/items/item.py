da9001d650f9fcce92439c6e13ef00e9a8a71c04
jamiesmagnolia <jamie.a.magnolia@gmail.com>
2024-11-07 00:15:28 +1100

    Add docstrings

--- src/items/item.py
+++ src/items/item.py
@@ -4,6 +4,11 @@ from abc import ABC
 
 
 class Item(ABC):
+    """
+    Item abstract base class. An Item refers to an in-game object that the player
+    can interact with. Portable items can be picked up (held and/or stored in the player's inventory)
+    or dropped while other items can't.'
+    """
     def __init__(self, name, desc, durability=1000, portable=True):
         self.name = name
         self.desc = desc
