d389d00f54c7b190f6a1ccfaf03e621767833077
jamiesmagnolia <jamie.a.magnolia@gmail.com>
2024-11-06 16:36:51 +1100

    Update Player class - Add str and repr methods - Add display_stats() method - Add pickup/drop_item() methods

--- src/player.py
+++ src/player.py
@@ -1,4 +1,25 @@
 class Player:
 
-    def __init__(self):
-        pass
+    def __init__(self, name, hp=1000, stamina=1000 // 2):
+        self.name = name
+        self.hp = hp
+        self.stamina = stamina
+        self.inventory = []
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"Player\nName: {self.name}\nHP: {self.hp}\nStamina: {self.stamina}\nInventory: {self.inventory}"
+
+    def display_stats(self):
+        print("Stats: ")
+        print("HP: ", self.hp)
+        print("Stamina: ", self.stamina)
+        print("Inventory:", len(self.inventory))
+
+    def pickup_item(self, new_item):
+        self.inventory.append(new_item)
+
+    def drop_item(self, item_index):
+        self.inventory.pop(item_index - 1)
