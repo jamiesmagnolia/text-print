c6def8ab12261c8727c4265dc598077090d2b3f5
jamiesmagnolia <jamie.a.magnolia@gmail.com>
2024-11-07 00:16:19 +1100

    Add menu_desc method

--- src/actions/move_action.py
+++ src/actions/move_action.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from actions.action import Action
+from player import Player
 from room import Room
 
 
@@ -12,11 +13,18 @@ class MoveAction(Action):
     different available rooms at that current room.
     """
 
-    def __init__(self, target_room: Room):
+    def __init__(self, target_room: 'Room'):
         self.target_room = target_room
 
-    def execute(self, player, game):
+    def execute(self, player: 'Player'):
         """
         Navigate to the target room.
         """
-        game.current_room = self.target_room
+        pass
+        #
+        # from game import Game
+        # game.current_room = self.target_room
+        # return self.menu_desc(player)
+
+    def menu_desc(self, actor) -> str:
+        return f"{actor} heads towards {self.target_room.name}."
