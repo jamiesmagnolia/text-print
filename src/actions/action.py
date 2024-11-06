1f50152bd1fe4e33efbcd48739d9b98e2ec8f35b
jamiesmagnolia <jamie.a.magnolia@gmail.com>
2024-11-06 18:56:39 +1100

    Add typehints and comments

--- src/actions/action.py
+++ src/actions/action.py
@@ -1,4 +1,8 @@
+from __future__ import annotations
+
 from abc import ABC, abstractmethod
+
+from player import Player
 
 
 class Action(ABC):
@@ -7,5 +11,11 @@ class Action(ABC):
     """
 
     @abstractmethod
-    def execute(self):
+    def execute(self, player: Player, game) -> None:  # TODO: return string desc of action execution?
+        """
+        Executes the action.
+        :param player: The player to execute the action.
+        :param game: The game to play on.
+        :return:
+        """
         pass
