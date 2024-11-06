b63c3631ed98291c0f12d6843a954fc3adb6199b
jamiesmagnolia <jamie.a.magnolia@gmail.com>
2024-11-06 19:58:01 +1100

    Remove game logic - Replace with Game instance and run()

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
-import os
-
+from game import Game
 from player import Player
 from room import Room
 
@@ -14,54 +13,16 @@ room2 = Room("Living Room", "Ignoring the single worn out couch, it looks quite 
 room3 = Room("Kitchen", "Slightly more homey compared to the rest of the rooms")
 room4 = Room("Backyard", "Nothing but grass and a single mango tree.")
 
+rooms = [room1, room2, room3, room4]
+
 # Connecting rooms to each other
 room1.add_exit(room2)
 room2.add_exits([room1, room3, room4])
 room3.add_exit(room2)
 room4.add_exit(room2)
 
-
-# Util functions
-def clear_screen():
-    # Clear command for Windows vs Unix-based systems (Linux, macOS)
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-
-# Game loop starts
-def game_loop():
-    # Starting room
-    current_room = room1
-    while True:
-        # Start every choice with a cleared terminal
-        clear_screen()
-
-        print(f"\nYou're in {current_room}.")
-        print(current_room.desc)
-
-        # Player stats
-        player.display_stats()
-
-        print("\nWhere will you go next?")
-        # Display navigation options
-        for i, room, in enumerate(current_room.exits, 1):
-            print(f"{i}. Go to {room}")
-
-        # Getting player input
-        choice = input("Select option (or 'q' to quit/exit): ")
-
-        # Handle player input
-        if choice.isdigit():
-            choice = int(choice) - 1
-            if 0 <= choice < len(current_room.exits):
-                current_room = current_room.exits[choice]
-            else:
-                print("That's not a valid option.")
-        elif choice == 'q':
-            print("Thanks for playing!")
-            break
-        else:
-            print("That's not a valid option.")
-
+# Create Game
+game = Game(player, rooms)
 
 # Start game
-game_loop()
+game.run()
