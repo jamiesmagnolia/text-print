6e3a3effbe0ca543e2ea800714d2a069eaf34545
jamiesmagnolia <jamie.a.magnolia@gmail.com>
2024-11-06 23:10:16 +1100

    Add menu_dec() abstractmethod and remove imports - Imports cause circular imports - Typehints will now use '' instead

--- src/actions/action.py
+++ src/actions/action.py
@@ -2,8 +2,6 @@ from __future__ import annotations
 
 from abc import ABC, abstractmethod
 
-from player import Player
-
 
 class Action(ABC):
     """
@@ -11,11 +9,20 @@ class Action(ABC):
     """
 
     @abstractmethod
-    def execute(self, player: Player, game) -> None:  # TODO: return string desc of action execution?
+    def execute(self, player: 'Player', game: 'Game') -> str:
         """
         Executes the action.
         :param player: The player to execute the action.
         :param game: The game to play on.
-        :return:
+        :return: A description of the executed action.
+        """
+        pass
+
+    @abstractmethod
+    def menu_desc(self, actor) -> str:
+        """
+        Describes the action that could be performed shown on the menu page.
+        :param actor:
+        :return: A description of the action shown on the menu page.
         """
         pass
