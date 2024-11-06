f33257e61dec6961ee47bd75673afb06b2a7838f
jamiesmagnolia <jamie.a.magnolia@gmail.com>
2024-11-07 00:15:48 +1100

    Add constructor and display method

--- src/menu.py
+++ src/menu.py
@@ -1,3 +1,11 @@
+from __future__ import annotations
+
+from actions.action import Action
+
+
 class Menu:
-    def __init__(self):
+    def __init__(self, actions: list[Action]):
+        self.actions = actions
+
+    def display(self) -> None:
         pass
