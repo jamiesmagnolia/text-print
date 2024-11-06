2419f639efcc4cc5d26b2ee5a315341b89a3b13b
jamiesmagnolia <jamie.a.magnolia@gmail.com>
2024-11-06 20:00:09 +1100

    Add typehints and docstrings

--- src/room.py
+++ src/room.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class Room:
     """
     Room class.
@@ -9,27 +12,33 @@ class Room:
     the Player can explore.
     """
 
-    def __init__(self, name="Unknown", desc="An unexplored room."):
+    def __init__(self, name: str = "Unknown", desc: str = "An unexplored room."):
         """
         Create a Room object.
         :param name: The name of the room.
         :param desc: The description of the room.
+
+        Attributes:
+        name: The name of the room.
+        desc: The description of the room.
+        exits: The exits of the room (which room the player can navigate to next).
+        items: Player Inventory list / A list of items the Player currently holds.
         """
         self.name = name
         self.desc = desc + "\n"
         self.exits = []
         self.items = []
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
     def __repr__(self):
-        result = f"Room: ('{self.name}')\nDescription: ('{self.desc}')\nExits: ('{self.exits}')"
+        return f"Room: ('{self.name}')\nDescription: ('{self.desc}')\nExits: ('{self.exits}')"
 
-    def add_exit(self, new_exit):
-        """Add a single exit"""
+    def add_exit(self, new_exit: Room) -> None:
+        """Add a single exit to the room's current exits"""
         self.exits.append(new_exit)
 
-    def add_exits(self, new_exits):
+    def add_exits(self, new_exits: list[Room]) -> None:
         """Add a list of exits to the room's current exits"""
         self.exits.extend(new_exits)
